8db0f505cc29190ead3c90f13b0641db24b80428
Mojo <moraels@outlook.com>
2019-08-22 16:03:10 +0100

    Added comments

--- for.py
+++ for.py
@@ -13,11 +13,13 @@
 # print(f"There are {consonants} consonants.")
 
 students = {
-    "male":["Adam", "Bob", "Charlie", "Donald"],
-    "female":["Alice", "Bobby", "Clare", "Diane"]
+    "male":["Adam", "Bob", "Charlie", "Donald", "Grant"],
+    "female":["Alice", "Bobby", "Clare", "Diane", "Grace"]
 }
 
 for key in students.keys():
     for name in students[key]:
         if "a" in name:
             print(name)
+
+#this program shows you how many vowels and consonents are in the students names
