f9123e439ce382c2687a757594939ea5a0cdabd6
bodgerBader <54405135+bodgerBader@users.noreply.github.com>
2019-08-22 14:42:33 +0100

    Add files via upload

---
+++ health.py
@@ -0,0 +1,19 @@
+import random
+
+
+hp = 50
+
+difficulty = 2
+
+health_potion = int(random.randint(25, 50) / difficulty)
+
+hp = hp + health_potion
+
+print(f'Your health is now: {hp}')
+
+#original print
+print(f'Your health is now: {hp}, while at difficulty level: {difficulty}')   
+
+#working with difficulty thanks to josh
+print("Your health is now {hp}, while difficulty level: {difficulty}".format(hp=hp, difficulty=difficulty))
+
